a985e0c323d0f1113f8e1ddc1f423851fb3bff0c
AbdulRehman385 <abdulrehmanshaikh787@gmail.com>
2025-02-04 12:28:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,44 +16,81 @@ import numpy as np
 model = load_model("model_cnn.h5")
 
 # Predict Model
+# def process_and_predict(file):
+#     im = Image.open(file).convert("RGB")
+#     width, height = im.size
+#     if width == height:
+#         im = im.resize((200,200), Image.LANCZOS)
+#     else:
+#         if width > height:
+#             left = int((width - height) / 2)
+#             right = int((width + height) / 2)
+#             top = 0
+#             bottom = height
+#             im = im.crop((left,top,right,bottom))
+#             im = im.resize((200,200), Image.LANCZOS)
+#         else:
+#             left = 0
+#             right = width
+#             top = 0
+#             bottom = width
+#             im = im.crop((left,top,right,bottom))
+#             im = im.resize((200,200), Image.LANCZOS)
+
+#     ar = np.asarray(im)
+#     ar = ar.astype('float32')
+#     ar /= 255.0
+#     ar = ar.reshape(-1, 200, 200, 3)
+
+#     pred = model.predict(ar)
+
+#     # Get the index of the highest probability
+#     pred_class = np.argmax(pred, axis=-1)  # This returns the index of the highest probability
+
+#     class_index = pred_class[0]
+    
+#     class_labels = ['Healthy (No Tumor)', 'Pituitary', 'Meningioma', 'Glioma']
+#     prediction = class_labels[class_index]
+
+#     # print(f"Type of tumor: {prediction}")
+#     return prediction ,im.resize((300,300), Image.LANCZOS)
+
+
 def process_and_predict(file):
     im = Image.open(file).convert("RGB")
     width, height = im.size
-    if width == height:
-        im = im.resize((200,200), Image.LANCZOS)
-    else:
-        if width > height:
-            left = width/2 - height/2
-            right = width/2 + height/2
-            top = 0
-            bottom = height
-            im = im.crop((left,top,right,bottom))
-            im = im.resize((200,200), Image.LANCZOS)
-        else:
-            left = 0
-            right = width
-            top = 0
-            bottom = width
-            im = im.crop((left,top,right,bottom))
-            im = im.resize((200,200), Image.LANCZOS)
 
-    ar = np.asarray(im)
-    ar = ar.astype('float32')
-    ar /= 255.0
+    # Resize while maintaining aspect ratio
+    if width != height:
+        if width > height:
+            left = int((width - height) / 2)
+            right = int((width + height) / 2)
+            im = im.crop((left, 0, right, height))
+        else:
+            top = int((height - width) / 2)
+            bottom = int((height + width) / 2)
+            im = im.crop((0, top, width, bottom))
+
+    im = im.resize((200, 200), Image.LANCZOS)
+    
+    # Normalize and reshape for CNN
+    ar = np.asarray(im).astype('float32') / 255.0
     ar = ar.reshape(-1, 200, 200, 3)
 
+    # Predict
     pred = model.predict(ar)
 
-    # Get the index of the highest probability
-    pred_class = np.argmax(pred, axis=-1)  # This returns the index of the highest probability
-
-    class_index = pred_class[0]
+    # Get predicted class
+    pred_class = np.argmax(pred, axis=-1)  # Returns array, needs conversion
+    class_index = int(pred_class[0])  # Convert to integer
     
+    # Map predicted class to labels
     class_labels = ['Healthy (No Tumor)', 'Pituitary', 'Meningioma', 'Glioma']
     prediction = class_labels[class_index]
 
-    # print(f"Type of tumor: {prediction}")
-    return prediction ,im.resize((300,300), Image.LANCZOS)
+    return prediction, im.resize((300, 300), Image.LANCZOS)
+
+
 
 # Title
 st.title('Brain Tumor Classification App')
